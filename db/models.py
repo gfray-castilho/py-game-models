b3582ed4124157a151fe33bdaa750ba3ace80f2d
Gabriel <gfraycastilho@gmail.com>
2025-10-29 00:47:10 -0300

    solved

--- db/models.py
+++ db/models.py
@@ -4,7 +4,8 @@ from django.db import models
 class Race(models.Model):
     name = models.CharField(max_length=255, unique=True)
     description = models.TextField(blank=True)
-    def __str__(self):
+
+    def __str__(self) -> str:
         return self.name
 
 
@@ -12,23 +13,29 @@ class Skill(models.Model):
     name = models.CharField(max_length=255, unique=True)
     bonus = models.CharField(max_length=255)
     race = models.ForeignKey(Race, on_delete=models.CASCADE)
-    def __str__(self):
-        return self.name + " " + self.bonus + " " + str(self.race)
+
+    def __str__(self) -> str:
+        return f"{self.name} {self.bonus} {self.race}"
 
 
 class Guild(models.Model):
     name = models.CharField(max_length=255, unique=True)
     description = models.TextField(null=True)
-    def __str__(self):
-        return self.name + " " + self.description
+
+    def __str__(self) -> str:
+        return f"{self.name} {self.description}"
 
 
 class Player(models.Model):
-    nickname = models.CharField(max_length=255, unique =True)
-    email = models.EmailField(max_length=255, unique = False)
+    nickname = models.CharField(max_length=255, unique=True)
+    email = models.EmailField(max_length=255)
     bio = models.CharField(max_length=255)
     race = models.ForeignKey(Race, on_delete=models.CASCADE)
     guild = models.ForeignKey(Guild, on_delete=models.SET_NULL, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    def __str__(self):
-        return self.nickname + " " + self.email + " " + self.bio + " " + str(self.race) + " " + str(self.guild) + " " + str(self.created_at)
+
+    def __str__(self) -> str:
+        return (
+            f"{self.nickname} {self.email} {self.bio} "
+            f"{self.race} {self.guild} {self.created_at}"
+        )
--- main.py
+++ main.py
@@ -4,34 +4,37 @@ from db.models import Race, Skill, Player, Guild
 
 
 def main() -> None:
-    with open ("players.json", "r") as file:
-        dadosJson = json.load(file)
+    with open("players.json", "r") as file:
+        dados_json = json.load(file)
 
-    for nickname, dados in dadosJson.items():
+    for nickname, dados in dados_json.items():
         race, _ = Race.objects.get_or_create(
             name=dados["race"]["name"],
-            defaults={"description": dados["race"]["description"]})
+            defaults={"description": dados["race"]["description"]},
+        )
 
         for skill_data in dados["race"]["skills"]:
-            skill, _ = Skill.objects.get_or_create(
+            Skill.objects.get_or_create(
                 name=skill_data["name"],
                 bonus=skill_data["bonus"],
-                race=race)
+                race=race,
+            )
 
         if dados["guild"]:
             desc = dados["guild"]["description"]
             guild, _ = Guild.objects.get_or_create(
                 name=dados["guild"]["name"],
-                defaults={"description": desc})
+                defaults={"description": desc},
+            )
         else:
             guild = None
 
-        player = Player.objects.create(
+        Player.objects.create(
             nickname=nickname,
             email=dados["email"],
             bio=dados["bio"],
             race=race,
-            guild=guild
+            guild=guild,
         )
 
 
